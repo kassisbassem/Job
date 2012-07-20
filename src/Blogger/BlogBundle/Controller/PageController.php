13a76244e67a4bb851363c0d8f42093ebed756fb
bassem <bassem@wided-limem>
2012-07-20 15:40:19 +0100

    this is my first commit

---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:index.html.twig');
+    }
+}
